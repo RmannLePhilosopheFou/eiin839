e28ed8186ea4d9c5d59a35c75e0267e261d3df3b
Jean Armand <jean-armand.honyigloh@etu.univ-cotedazur.fr>
2021-02-20 23:17:56 +0100

    Mise à jour

---
+++ TD2/HttpListener/BasicServerHttpListener/Header.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Net;
+
+
+namespace HeaderRequest
+{
+    class Header
+    {
+        private HttpListenerRequest httpListnerRequest;
+
+        public Header(HttpListenerRequest request)
+        {
+            this.httpListnerRequest = request;
+        }
+
+        public void displayAllHeader() //Affiche tous les types d'entêtes contenues dans l'enum HttpRequestHeader
+        {
+           Console.WriteLine("here");
+           foreach (string str in Enum.GetNames(typeof(HttpRequestHeader)))
+                {
+                Console.WriteLine(" "+ str+ ", "+this.httpListnerRequest.Headers[str]);
+            }
+        }
+
+        public void printOneHeader(HttpRequestHeader header)
+        {
+            Console.WriteLine(httpListnerRequest.Headers[header.ToString()]);
+        }
+
+       
+    }
+}
